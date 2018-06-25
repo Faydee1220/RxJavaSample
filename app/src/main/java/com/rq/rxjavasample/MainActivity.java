8f1d466e64b306ff44cac2c74edab91803b2c1dd
Faydee <faydee1220@gmail.com>
2018-06-25 13:08:59 +0800

    add onClickListener
    use Java 1.8

--- app/src/main/java/com/rq/rxjavasample/MainActivity.java
+++ app/src/main/java/com/rq/rxjavasample/MainActivity.java
@@ -3,14 +3,25 @@ package com.rq.rxjavasample;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import com.rq.rxjavasample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+  private static final String TAG = MainActivity.class.getSimpleName();
+
+  private ActivityMainBinding mBinding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    ActivityMainBinding binding = DataBindingUtil
+    mBinding = DataBindingUtil
         .setContentView(this, R.layout.activity_main);
+    mBinding.buttonMainUpdate.setOnClickListener(view -> updateButtonPressed());
+
+  }
+
+
+  private void updateButtonPressed() {
+    Log.d(TAG, "updateButtonPressed");
   }
 }
