719764a534f4eaf5cdd283aaf017f93af05e0e36
Faydee <faydee1220@gmail.com>
2018-06-25 11:30:03 +0800

    add DataBinding

--- app/src/main/java/com/rq/rxjavasample/MainActivity.java
+++ app/src/main/java/com/rq/rxjavasample/MainActivity.java
@@ -1,13 +1,16 @@
 package com.rq.rxjavasample;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.rq.rxjavasample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    ActivityMainBinding binding = DataBindingUtil
+        .setContentView(this, R.layout.activity_main);
   }
 }
